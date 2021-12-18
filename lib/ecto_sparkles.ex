d6da41fd412a40ad10e2ddc88818292bdc1d6c76
Mayel de Borniol <git@mayel.space>
2021-12-19 11:19:26 +1300

    bring in query_filter from ecto_shorts

--- lib/ecto_sparkles.ex
+++ lib/ecto_sparkles.ex
@@ -1,11 +1,54 @@
+# SPDX-License-Identifier: Apache-2.0
 defmodule EctoSparkles do
   import Ecto, only: [assoc: 2]
 
+  @moduledoc """
+  `query_filter` brings convenience and shortens the boilterplate of ecto queries
+
+  Common filters available include:
+
+  - `preload` - Preloads fields onto the query results
+  - `start_date` - Query for items inserted after this date
+  - `end_date` - Query for items inserted before this date
+  - `before` - Get items with IDs before this value
+  - `after` - Get items with IDs after this value
+  - `ids` - Get items with a list of ids
+  - `first` - Gets the first n items
+  - `last` - Gets the last n items
+  - `limit` - Gets the first n items
+  - `offset` - Offsets limit by n items
+  - `search` - ***Warning:*** This requires schemas using this to have a `&by_search(query, val)` function
+
+  You are also able to filter on any natural field of a model, as well as use
+
+  - gte/gt
+  - lte/lt
+  - like/ilike
+  - is_nil/not(is_nil)
+
+  ```elixir
+  query_filter(User, %{name: %{ilike: "steve"}})
+  query_filter(User, %{name: %{ilike: "steve"}}, :last_name, :asc)
+  query_filter(User, %{name: %{age: %{gte: 18, lte: 30}}})
+  query_filter(User, %{name: %{is_banned: %{!=: nil}}})
+  query_filter(User, %{name: %{is_banned: %{==: nil}}})
+
+  my_query = query_filter(User, %{name: "Billy"})
+  query_filter(my_query, %{last_name: "Joe"})
+  ```
+  """
+
+  def query_filter(module_or_query, filters, order_by_prop \\ :id, order_direction \\ :desc) do
+    EctoSparkles.Filter.query_params(module_or_query, filters, order_by_prop, order_direction)
+  end
+
+
   @doc """
-  A helper for preloading associations using joins.
-  Based on https://hexdocs.pm/ecto_preloader (licensed under WTFPL)
+  `join_preload` is a helper for preloading associations using joins.
+
   By default, Ecto preloads associations using a separate query for each association, which can degrade performance.
   You could make it run faster by using a combination of join/preload, but that requires a bit of boilerplate (see example below).
+
   With `EctoSparkles`, you can accomplish this with just one line of code.
 
   ## Example using just Ecto
@@ -20,10 +63,9 @@ defmodule EctoSparkles do
   |> Repo.all()
   ```
 
-  ## Example using Ecto.Preloader
+  ## Example using `join_preload`
 
   ```
-  import Ecto.Query
   import EctoSparkles
   Invoice
   |> join_preload([:customer, :account])
@@ -31,10 +73,10 @@ defmodule EctoSparkles do
   |> Repo.all()
   ```
   """
-  defmacro join_preload(query, associations), do: preload_join(query, associations)
+  defmacro join_preload(query, associations), do: do_join_preload(query, associations)
 
-  defp preload_join(query, []), do: query
-  defp preload_join(query, associations) do
+  defp do_join_preload(query, []), do: query
+  defp do_join_preload(query, associations) do
     root = Macro.var(:root, __MODULE__)
     cond do
       is_list(associations) ->
@@ -50,9 +92,9 @@ defmodule EctoSparkles do
         |> preload_clause(preload, preload)
       true ->
         raise RuntimeError,
-          "expected atom or list of atoms, got: #{inspect(associations)}"
+          "join_preload expected an atom or list of atoms, got: #{inspect(associations)}"
     end
-  end    
+  end
 
   defp joins(query, [], _bindings, _assoc), do: query
   defp joins(query, [j | js], bindings, assoc) do
@@ -62,7 +104,7 @@ defmodule EctoSparkles do
     join_clause(query, bindings, condition, [as: j], j)
     |> joins(js, bs, j)
   end
-    
+
   # [a: a], [a: a, b: b], [a: a, b: b, c: c] etc.
   defp preload_bindings(names), do: Enum.map(names, &{&1, Macro.var(&1, __MODULE__)})
 
@@ -77,8 +119,9 @@ defmodule EctoSparkles do
   defp preload_clause(query, bindings, expr),
     do: quote(do: Ecto.Query.preload(unquote(query), unquote(bindings), unquote(expr)))
 
+
   @doc """
-  Similar to `Ecto.Query.join/{4,5}`, but can be called multiple times with the same alias.
+  `reusable_join` is similar to `Ecto.Query.join/{4,5}`, but can be called multiple times with the same alias.
 
   Note that only the first join operation is performed, the subsequent ones that use the same alias
   are just ignored. Also note that because of this behaviour, it is mandatory to specify an alias when
---
+++ lib/release_tasks.ex
@@ -0,0 +1,46 @@
+# SPDX-License-Identifier: Apache-2.0
+defmodule EctoSparkles.ReleaseTasks do
+  @app Bonfire.Common.Config.get!(:otp_app)
+
+  def rollback(repo \\ nil, step \\ 1)
+
+  def migrate(repo) do
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+  end
+
+  def rollback(repo, step) when not is_nil(repo) do
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, step: step))
+  end
+
+  def rollback_to(repo, version) do
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  def rollback_all(repo) do
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, all: true))
+  end
+
+
+  def migrate do
+    for repo <- repos(), do: migrate(repo)
+  end
+
+  def rollback(nil, step) do
+    for repo <- repos(), do: rollback(repo, step)
+  end
+
+  def rollback_to(version) do
+    for repo <- repos(), do: rollback_to(repo, version)
+  end
+
+  def rollback_all() do
+    for repo <- repos(), do: rollback_all(repo)
+  end
+
+
+  defp repos do
+    Application.load(@app)
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+
+end
