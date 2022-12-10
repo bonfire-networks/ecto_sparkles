4c1e66a4191bd461369c47c507de3adbce0f45e2
Mayel de Borniol <git@mayel.space>
2022-12-10 13:39:28 +1300

    add NPlus1Detector

--- lib/log.ex
+++ lib/log.ex
@@ -39,19 +39,19 @@ defmodule EctoSparkles.Log do
       )
       when (is_nil(source) or source not in @exclude_sources) and
              query not in @exclude_queries do
-    maybe_handle_event(measurements, metadata)
+    do_handle_event(measurements, metadata)
   end
 
   def handle_event(_, measurements, metadata, config) do
-    # IO.inspect(metadata, label: "EctoSparkles: unhandled ecto log")
+    # IO.inspect(metadata, label: "EctoSparkles: ignoring ecto log")
     nil
   end
 
-  defp maybe_handle_event(
+  defp do_handle_event(
          %{query_time: query_time, decode_time: decode_time} = measurements,
          %{query: query, source: source} = metadata
        ) do
-    maybe_trace(
+    check_if_slow(
       System.convert_time_unit(query_time, :native, :millisecond) +
         System.convert_time_unit(decode_time, :native, :millisecond),
       measurements,
@@ -59,23 +59,23 @@ defmodule EctoSparkles.Log do
     )
   end
 
-  defp maybe_handle_event(
+  defp do_handle_event(
          %{query_time: query_time} = measurements,
          %{query: query, source: source} = metadata
        ) do
-    maybe_trace(
+    check_if_slow(
       System.convert_time_unit(query_time, :native, :millisecond),
       measurements,
       metadata
     )
   end
 
-  defp maybe_handle_event(measurements, metadata) do
+  defp do_handle_event(measurements, metadata) do
     {result, _} = metadata.result
     log_query(result, nil, measurements, metadata)
   end
 
-  def maybe_trace(duration_in_ms, measurements, %{query: query} = metadata)
+  defp check_if_slow(duration_in_ms, measurements, %{query: query} = metadata)
       when duration_in_ms > 10 do
     slow_definition_in_ms = Bonfire.Common.Config.get([Bonfire.Common.Repo, :slow_query_ms], 100)
 
@@ -91,7 +91,7 @@ defmodule EctoSparkles.Log do
     end
   end
 
-  def maybe_trace(duration_in_ms, measurements, metadata) do
+  defp check_if_slow(duration_in_ms, measurements, metadata) do
     {result, _} = metadata.result
     log_query(result, duration_in_ms, measurements, metadata)
   end
@@ -100,8 +100,7 @@ defmodule EctoSparkles.Log do
       when result in [:error, "error"] do
     if not String.contains?(metadata.query, @exclude_match),
       do:
-        Logger.log(
-          :error,
+        Logger.error(
           "SQL query: " <>
             format_log(result, duration_in_ms, measurements, metadata)
         )
@@ -110,13 +109,35 @@ defmodule EctoSparkles.Log do
   def log_query(result, duration_in_ms, measurements, metadata) do
     level = String.to_atom(System.get_env("DB_QUERIES_LOG_LEVEL", "debug"))
 
-    if level && not String.contains?(metadata.query, @exclude_match),
-      do:
-        Logger.log(
+    if level && not String.contains?(metadata.query, @exclude_match) do
+      count_n_plus_1 = check_if_n_plus_1(metadata.query)
+
+      cond do
+        is_integer(count_n_plus_1) ->
+          Logger.warning(
+            "---------> Possible n+1 query detected! Number of occurrences: #{count_n_plus_1} SQL query: " <>
+              format_log(result, duration_in_ms, measurements, metadata)
+          )
+
+        not is_nil(level) ->
+         Logger.log(
           level,
           "SQL query: " <>
             format_log(result, duration_in_ms, measurements, metadata)
-        )
+          )
+
+        true -> # skip
+      end
+    end
+  end
+
+  def check_if_n_plus_1(query) do
+    case EctoSparkles.NPlus1Detector.check(query) do
+      {:match, count} ->
+          count
+      _ ->
+        # no match
+    end
   end
 
   def format_log(result, duration_in_ms, measurements, metadata) do
@@ -128,7 +149,16 @@ defmodule EctoSparkles.Log do
 
     source = if metadata.source, do: "source=#{inspect(metadata.source)}"
 
-    "#{result} db=#{duration_in_ms}ms #{source}\n  #{query} \n  params=#{params}"
+    # IO.inspect(metadata)
+    stacktrace = case metadata[:stacktrace] do
+      stacktrace when is_list(stacktrace) ->
+        stacktrace
+        |> Enum.slice(2, 2)
+        |> Exception.format_stacktrace()
+      _ -> nil
+    end
+
+    "#{result} db=#{duration_in_ms}ms #{source}\n  #{query} \n  params=#{params} \n#{stacktrace}"
   end
 
   defp decode_value(value) when is_list(value) do
---
+++ lib/n_plus_1_detector.ex
@@ -0,0 +1,34 @@
+defmodule EctoSparkles.NPlus1Detector do
+  @moduledoc """
+  Checks a query against the previous one and increments counter of collisions or swaps previous query with the last one.
+  """
+
+  # Increase counter or swaps query
+  def check("SELECT" <> _rest = query) do
+    do_check(query, get(query))
+  end
+
+  def check(_query) do
+    nil
+  end
+
+  defp get(query) do
+    Process.get("one_plus_n_detector: #{query}")
+  end
+
+  defp put(query, counter) do
+    Process.put("one_plus_n_detector: #{query}", counter)
+  end
+
+  def do_check(query, nil) do
+    put(query, 1)
+    :first
+  end
+
+  def do_check(query, counter) do
+    counter = counter + 1
+    put(query, counter)
+    {:match, counter}
+  end
+
+end
