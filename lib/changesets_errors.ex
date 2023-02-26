867f66dbbde3461a488516b735c7be8d6c02253c
Mayel de Borniol <git@mayel.space>
2023-02-26 21:32:19 +1300

    err

--- lib/changesets_errors.ex
+++ lib/changesets_errors.ex
@@ -10,8 +10,10 @@ defmodule EctoSparkles.Changesets.Errors do
   def changeset_errors_string(%Ecto.Changeset{} = changeset, _) do
     # IO.inspect(changeset)
     Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
-      Enum.reduce(opts, msg, fn {key, value}, acc ->
-        String.replace(acc, "%{#{key}}", do_to_string(value))
+      Enum.reduce(opts, msg, fn {key, value}, 
+      acc ->
+        do_to_string(acc) 
+        |> String.replace("%{#{do_to_string(key)}}", do_to_string(value))
       end)
     end)
     |> enum_errors()
@@ -30,6 +32,7 @@ defmodule EctoSparkles.Changesets.Errors do
 
   defp do_to_string(val, sep \\ ", ")
 
+
   defp do_to_string(val, sep) when is_list(val) do
     Enum.map(val, &do_to_string/1)
     |> Enum.filter(& &1)
@@ -37,6 +40,8 @@ defmodule EctoSparkles.Changesets.Errors do
   end
 
   defp do_to_string(empty, _) when empty == %{} or empty == "", do: nil
+  defp do_to_string(val, _sep) when is_binary(val), do: val
+  defp do_to_string({a, b}, _sep), do: "{#{do_to_string(a)}, #{do_to_string(b)}}"
   defp do_to_string(%{} = enum_errors, _), do: enum_errors(enum_errors)
   defp do_to_string(val, _), do: to_string(val)
 end
