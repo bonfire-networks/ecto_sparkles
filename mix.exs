ca84b47d505bff545317aa5a61803769c2095c47
Mayel de Borniol <git@mayel.space>
2024-02-24 19:13:45 +0000

    up

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoSparkles.Project do
   def project do
     [
       app: :ecto_sparkles,
-      version: "0.1.2",
+      version: "0.1.3",
       description: "Helper library to better join + preload Ecto associations, and other goodies",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
